67bb60da44a6e1fbcb3e23cb9a196fc3db2d3a67
InternalServerError98 <soumadeepbasu98@gmail.com>
2025-05-12 18:23:03 +0530

    routing/paging/loading

--- app/dashboard/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function page(){
-
-
-    return <p>This is the template for the Dashboard</p>;
-}
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,8 +1,48 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { Query } from '@neondatabase/serverless';
+import { useSearchParams, useRouter, usePathname } from 'next/navigation';
+import { use } from 'react';
+import {useDebouncedCallback} from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathName = usePathname();
+
+  const handleSearch = useDebouncedCallback((searchTerm: string) => {
+
+    const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
+    if(searchTerm) {
+      params.set('query', searchTerm);
+    }
+    else{
+      params.delete('query');
+    }
+    router.replace(`${pathName}?${params.toString()}`);
+
+
+
+  }, 300);
+
+  // function handleSearch(searchTerm: string) {
+  //   // Handle search logic here
+  //   const params = new URLSearchParams(searchParams);
+    
+  //   if(searchTerm) {
+  //     params.set('query', searchTerm);
+  //   }
+  //   else{
+  //     params.delete('query');
+  //   }
+  //   router.replace(`${pathName}?${params.toString()}`);
+
+  // }
+
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,6 +51,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={(e) => handleSearch(e.target.value)}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
