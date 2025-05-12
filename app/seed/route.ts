5ba02da9a418759bf348c87d713f55a47f958c2b
InternalServerError98 <soumadeepbasu98@gmail.com>
2025-05-12 12:35:26 +0530

    updating postgres connection type part 2

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,10 +1,13 @@
 import bcryptjs from 'bcryptjs';
 import postgres from 'postgres';
+// import { neon } from '@neondatabase/serverless';
 import { invoices, customers, revenue, users } from '../lib/placeholder-data';
 
 // const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
-const sql = postgres(process.env.DATABASE_URL!,  { ssl: 'verify-full' });
+//const sql = neon(process.env.DATABASE_URL!);
+
+const sql = postgres(process.env.DATABASE_URL!, { ssl: 'require' });
 
 async function seedUsers() {
   await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
@@ -105,6 +108,58 @@ async function seedRevenue() {
 
 export async function GET() {
   try {
+
+
+    // const result = await sql.transaction([
+
+    //   sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`,
+    //   sql` CREATE TABLE IF NOT EXISTS users (
+    //         id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    //         name VARCHAR(255) NOT NULL,
+    //         email TEXT NOT NULL UNIQUE,
+    //         password TEXT NOT NULL
+    //       );`,
+    //   sql`${ () => {
+
+    //     var insertedInvoices = 
+    //       invoices.map(
+    //         (invoice) => sql`
+    //           INSERT INTO invoices (customer_id, amount, status, date)
+    //           VALUES (${invoice.customer_id}, ${invoice.amount}, ${invoice.status}, ${invoice.date})
+    //           ON CONFLICT (id) DO NOTHING;
+    //         `,
+    //       );
+      
+    //     return insertedInvoices;
+        
+
+    //   }}`, 
+
+    //   sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`, 
+    //   sql`
+    //    CREATE TABLE IF NOT EXISTS customers (
+    //     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    //     name VARCHAR(255) NOT NULL,
+    //     email VARCHAR(255) NOT NULL,
+    //     image_url VARCHAR(255) NOT NULL
+    //   );
+    //   `,
+    //   sql`${ () => {
+
+    //     const insertedCustomers =
+    //       customers.map(
+    //         (customer) => sql`
+    //           INSERT INTO customers (id, name, email, image_url)
+    //           VALUES (${customer.id}, ${customer.name}, ${customer.email}, ${customer.image_url})
+    //           ON CONFLICT (id) DO NOTHING;
+    //         `,
+    //       );
+      
+    //     return insertedCustomers;
+
+    //   }}`, 
+
+    // ]);
     const result = await sql.begin((sql) => [
       seedUsers(),
       seedCustomers(),
